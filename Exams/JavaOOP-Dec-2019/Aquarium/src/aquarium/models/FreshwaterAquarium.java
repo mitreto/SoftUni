2f6508b069bcefed766ed61774954136d363dbed
mitre <dobrevdm@gmail.com>
2020-04-04 16:45:58 +0300

    remove

--- Exams/JavaOOP-Dec-2019/Aquarium/src/aquarium/models/FreshwaterAquarium.java
+++
@@ -1,9 +0,0 @@
-package aquarium.models;
-
-public class FreshwaterAquarium extends BaseAquarium {
-   // Has 50 capacity
-
-    public FreshwaterAquarium(String name, int capacity) {
-        super(name, capacity);
-    }
-}
--- Exams/JavaOOP-Dec-2019/Aquarium/src/aquarium/models/SaltwaterAquarium.java
+++
@@ -1,9 +0,0 @@
-package aquarium.models;
-
-public class SaltwaterAquarium extends BaseAquarium {
-    //Has 25 capacity
-
-    public SaltwaterAquarium(String name, int capacity) {
-        super(name, capacity);
-    }
-}
